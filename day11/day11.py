9b1a9048bd7c9bd49be66683f41751d7d00a64c2
Rilko <d.pfauntsch@gmail.com>
2023-12-11 10:12:52 +0100

    day 11

--- day11/day11.py
+++ day11/day11.py
@@ -1,40 +1,75 @@
 import pathlib
 import sys
 from collections import defaultdict
-import numpy as np
+from itertools import combinations
 
 
 def parse(parsedata):
-
     grid = defaultdict(str)
+    emptyrows = set()
+    emptycolumns = set()
+    galaxies = set()
 
-    count = 1
-    x, y = 0
-    for line in parsedata.splitlines():
-        for tile in line:
+    for y, line in enumerate(parsedata.splitlines()):
+
+        if line.count("#") == 0:
+            emptyrows.add(y)
+
+        for x, tile in enumerate(line):
+            grid[x, y] = tile
             if tile == "#":
-                grid[x, y]  = count
-                count +=1
-            else:
-                grid[x, y] = tile
+                galaxies.add((x, y))
 
-            x+=1
+    max_x = x
+    max_y = y
 
-        y += 1
+    for x in range(0, max_x + 1):
 
-    print(grid)
+        count = 0
+        for y in range(0, max_y + 1):
+            if grid[x, y] != ".":
+                count += 1
+
+        if count == 0:
+            emptycolumns.add(x)
+
+    return grid, galaxies, emptyrows, emptycolumns
 
 
-
-    return grid
+def expanded_man_dist(a, b, emptyrows, emptycolumns, expansion):
+    # off by one correction
+    if expansion > 1:
+        expansion = expansion - 1
+    dist = abs(a[1] - b[1]) + abs(a[0] - b[0])
+    passed_rows = set(range(*sorted((a[1], b[1]))))
+    passed_cols = set(range(*sorted((a[0], b[0]))))
+    for row in emptyrows:
+        if row in passed_rows:
+            dist += expansion
+    for col in emptycolumns:
+        if col in passed_cols:
+            dist += expansion
+    return dist
 
 
 def part1(data):
-    return ""
+    grid, galaxies, emptyrows, emptycolumns = data
+
+    pathsum = 0
+    for pairs in set(combinations(galaxies, 2)):
+        pathsum += expanded_man_dist(pairs[0], pairs[1], emptyrows, emptycolumns, expansion=1)
+
+    return pathsum
 
 
 def part2(data):
-    return ""
+    grid, galaxies, emptyrows, emptycolumns = data
+
+    pathsum = 0
+    for pairs in set(combinations(galaxies, 2)):
+        pathsum += expanded_man_dist(pairs[0], pairs[1], emptyrows, emptycolumns, expansion=1_000_000)
+
+    return pathsum
 
 
 def solve(puzzle_data):
