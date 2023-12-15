721db5e8f78d090a015a10b09001f5e377aacb05
Rilkon <58311175+Rilkon@users.noreply.github.com>
2023-12-15 17:23:02 +0100

    restructure

---
+++ csharp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+var solver6 = new SolveDay6("day06/input.txt");
+
+Console.WriteLine($"{solver6}");
+Console.WriteLine($"Part 1: {solver6.Part1()}");
+Console.WriteLine($"Part 2: {solver6.Part2()}");
+
+
+var solver15 = new SolveDay15("day15/input.txt");
+
