903f904b7afe28d60c9719bf1c17282d26aef6b7
Rilkon <58311175+Rilkon@users.noreply.github.com>
2023-12-15 17:27:11 +0100

    csharp

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -7,5 +7,5 @@ Console.WriteLine($"Part 1: {solver6.Part1()}");
 Console.WriteLine($"Part 2: {solver6.Part2()}");
 
 
-var solver15 = new SolveDay15("day15/input.txt");
+//var solver15 = new SolveDay15("day15/input.txt");
 
