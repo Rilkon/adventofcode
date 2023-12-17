d7a88a213bc5f0e78047b5b546d5d283e660e926
Rilkon <58311175+Rilkon@users.noreply.github.com>
2023-12-17 23:03:30 +0100

    .

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,13 +1,20 @@
 ﻿
 
-var solver6 = new SolveDay6("day06/input.txt");
 
-Console.WriteLine($"{solver6}");
-Console.WriteLine($"Part 1: {solver6.Part1()}");
-Console.WriteLine($"Part 2: {solver6.Part2()}");
+var solver = new SolveDay1("day01/input.txt");
+
+Console.WriteLine($"{solver}");
+Console.WriteLine($"Part 1: {solver.Part1()}");
+Console.WriteLine($"Part 2: {solver.Part2()}");
+
+// var solver6 = new SolveDay6("day06/input.txt");
+
+// Console.WriteLine($"{solver6}");
+// Console.WriteLine($"Part 1: {solver6.Part1()}");
+// Console.WriteLine($"Part 2: {solver6.Part2()}");
 
 
-var solver15 = new SolveDay15("day15/input.txt");
-Console.WriteLine($"{solver15}");
-Console.WriteLine($"Part 1: {solver15.Part1()}");
-Console.WriteLine($"Part 2: {solver15.Part2()}");
+// var solver15 = new SolveDay15("day15/input.txt");
+// Console.WriteLine($"{solver15}");
+// Console.WriteLine($"Part 1: {solver15.Part1()}");
+// Console.WriteLine($"Part 2: {solver15.Part2()}");
