15d730320626447ad87d61a2e1e639f34b525aa1
Rilkon <58311175+Rilkon@users.noreply.github.com>
2023-12-18 16:29:18 +0100

    day18 c#

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,11 +1,11 @@
 ﻿
 
 
-var solver = new SolveDay1("day01/input.txt");
+// var solver = new SolveDay1("day01/input.txt");
 
-Console.WriteLine($"{solver}");
-Console.WriteLine($"Part 1: {solver.Part1()}");
-Console.WriteLine($"Part 2: {solver.Part2()}");
+// Console.WriteLine($"{solver}");
+// Console.WriteLine($"Part 1: {solver.Part1()}");
+// Console.WriteLine($"Part 2: {solver.Part2()}");
 
 // var solver6 = new SolveDay6("day06/input.txt");
 
@@ -18,3 +18,10 @@ Console.WriteLine($"Part 2: {solver.Part2()}");
 // Console.WriteLine($"{solver15}");
 // Console.WriteLine($"Part 1: {solver15.Part1()}");
 // Console.WriteLine($"Part 2: {solver15.Part2()}");
+
+
+var solver = new SolveDay18("day18/input.txt");
+
+Console.WriteLine($"{solver}");
+Console.WriteLine($"Part 1: {solver.Part1()}");
+Console.WriteLine($"Part 2: {solver.Part2()}");
