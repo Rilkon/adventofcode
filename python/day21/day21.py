8c583a83d3f102d0ebe3cec92442e87f5a97067c
Rilkon <d.pfauntsch@gmail.com>
2023-12-21 16:06:24 +0100

    day 21 p2 start

--- python/day21/day21.py
+++ python/day21/day21.py
@@ -29,33 +29,33 @@ def parse(parsedata):
 
 
 def part1(data):
-    grid, start, max_x, max_y = data
-
-    q = deque()
-    q.append((start, 0))
-
-    visited = set()
-    goals = set()
-
-    while q:
-        curr, steps = q.popleft()
-
-        if steps == STEPGOAL:
-            goals.add(curr)
-            continue
-
-        if (curr, steps) in visited:
-            continue
-
-        visited.add((curr, steps))
-
-        for delta in DELTAS.values():
-            dy, dx = delta
-            x, y = curr
-            newpos = (x+dx, y+dy)
-            if newpos in grid.keys() and grid[newpos] != "#":
-                q.append((newpos, steps + 1))
-
+    # grid, start, max_x, max_y = data
+    #
+    # q = deque()
+    # q.append((start, 0))
+    #
+    # visited = set()
+    # goals = set()
+    #
+    # while q:
+    #     curr, steps = q.popleft()
+    #
+    #     if steps == STEPGOAL:
+    #         goals.add(curr)
+    #         continue
+    #
+    #     if (curr, steps) in visited:
+    #         continue
+    #
+    #     visited.add((curr, steps))
+    #
+    #     for delta in DELTAS.values():
+    #         dy, dx = delta
+    #         x, y = curr
+    #         newpos = (x+dx, y+dy)
+    #         if newpos in grid.keys() and grid[newpos] != "#":
+    #             q.append((newpos, steps + 1))
+    return ""
     return len(goals)
 
 
@@ -63,7 +63,61 @@ def part1(data):
 
 
 def part2(data):
+    grid, start, max_x, max_y = data
+
+
+
+    for value in [6, 10, 50, 100, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 5000, 6000, 7000, 8000]:
+        q = deque()
+        q.append((start, 0))
+
+        visited = set()
+
+        allgoals = {}
+        goals = set()
+        PART2GOAL = value
+
+        while q:
+            curr, steps = q.popleft()
+
+            if steps == PART2GOAL:
+                goals.add(curr)
+                continue
+
+            if (curr, steps) in visited:
+                continue
+
+            visited.add((curr, steps))
+
+            for delta in DELTAS.values():
+                dy, dx = delta
+                x, y = curr
+                newpos = (x+dx, y+dy)
+                if newpos in grid.keys():
+                    if grid[newpos] != "#":
+                        q.append((newpos, steps + 1))
+                else:
+                    x, y = newpos
+
+                    if x > max_x:
+                        x = x % (max_x+1)
+                    if y > max_y :
+                        y = y % (max_y+1)
+                    if x < 0:
+                        x = (x % (max_x+1) + max_x+1) % (max_x+1)
+                    if y < 0:
+                        y = (y % (max_y+1) + max_y+1) % (max_y+1)
+
+                    if grid[(x,y)] != "#":
+                        q.append((newpos, steps + 1))
+
+
+
+        allgoals[PART2GOAL] = len(goals)
+        print(allgoals)
+
     return ""
+    return allgoals
 
 
 def solve(puzzle_data):
