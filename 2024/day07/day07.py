e03ed104565bcc855637babe4d43c6a3923faac7
Rilkon <58311175+Rilkon@users.noreply.github.com>
2024-12-12 20:16:35 +0100

    AoC 2024 Day07

---
+++ 2024/day07/day07.py
@@ -0,0 +1,55 @@
+import pathlib
+import sys
+from copy import deepcopy
+
+
+def parse(parsedata):
+    calclist = []
+    for line in parsedata.splitlines():
+        entry = []
+
+        left, right = line.split(": ")
+        entry = [int(left), list(map(int, right.split(" ")))]
+        calclist.append(entry)
+
+    return calclist
+
+
+def elephant_math(result, nums, part2=False):
+    possible_results = {nums[0]}
+
+    for curr in nums[1:]:
+        temp = set()
+        for res in possible_results:
+            temp.add(res + curr)
+            temp.add(res * curr)
+            if part2:
+                temp.add(int(str(res) + str(curr)))
+        possible_results = temp
+
+    if result in possible_results:
+        return True
+    return False
+
+
+def part1(data):
+    return sum(left for left, right in data if elephant_math(left, right))
+
+
+def part2(data):
+    return sum(left for left, right in data if elephant_math(left, right, True))
+
+
+def solve(puzzle_data):
+    data = parse(puzzle_data)
+    solution1 = part1(deepcopy(data))
+    solution2 = part2(data)
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip()
+        solutions = solve(puzzle_input)
+        print("\n".join(str(solution) for solution in solutions))
