7b6e123d751c5229be686c3376d98fe159ef0f85
rilko <d.pfauntsch@gmail.com>
2025-11-29 15:51:08 +0100

    AoC2025 prep

---
+++ 2025/templates/template.py
@@ -0,0 +1,27 @@
+import pathlib
+import sys
+from copy import deepcopy
+
+
+def parse(parsedata):
+    return ""
+
+def part1(data):
+    return ""
+
+def part2(data):
+    return ""
+
+def solve(puzzle_data):
+    data = parse(puzzle_data)
+    solution1 = part1(deepcopy(data))
+    solution2 = part2(data)
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip()
+        solutions = solve(puzzle_input)
+        print("\n".join(str(solution) for solution in solutions))
